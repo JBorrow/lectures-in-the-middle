22603485731ffb9a6c5b1084e49cfca71daaacaf
Daniel <daniel.maitre@durham.ac.uk>
2017-03-10 07:34:11 +0000

    sorting tags

--- faq.py
+++ faq.py
@@ -18,7 +18,7 @@ for yy in y:
     tags.update(yy['tags'])
 
 with open(faq_dest+'/tags.yaml', 'w') as f:
-    f.write(yaml.dump(list(tags),default_flow_style=False))
+    f.write(yaml.dump(sorted(list(tags)),default_flow_style=False))
 
 #need to copy faq.yaml to compiled directory
 shutil.copyfile(faq_dir + '/faq.yaml', faq_dest +'/faq.yaml')
